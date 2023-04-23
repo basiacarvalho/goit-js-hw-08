20d5f811ff739edc4111a2d88f0410f8e9f875d3
basiacarvalho <basiacarvalho@gmail.com>
2023-04-23 20:33:31 +0200

    completed the 2nd and the 3rd tasks

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+const throttle = require('lodash.throttle');
 
+const formUpdate = function (event) {
+  const inputFormValues = {
+    email: document.querySelector('.feedback-form input').value,
+    message: document.querySelector('.feedback-form textarea').value,
+  };
+  const inputFormValuesJSON = JSON.stringify(inputFormValues);
+  localStorage.setItem('feedback-form-state', inputFormValuesJSON);
+};
+
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', throttle(formUpdate, 500));
+
+const formInputJSON = localStorage.getItem('feedback-form-state');
+const formInput = JSON.parse(formInputJSON);
+
+if (formInput !== null) {
+  document.querySelector('.feedback-form input').value = formInput.email;
+  document.querySelector('.feedback-form textarea').value = formInput.message;
+}
+
+const onSubmit = function (event) {
+  event.preventDefault();
+  localStorage.removeItem('feedback-form-state');
+  document.querySelector('.feedback-form input').value = '';
+  document.querySelector('.feedback-form textarea').value = '';
+  console.log(localStorage.getItem('feedback-form-state'));
+};
+
+form.addEventListener('submit', onSubmit);
